366346fc5a7c6208c624079172fc575c06b3a59b
Daniel Lima <daniellimaa05@gmail.com>
2022-11-21 17:11:36 -0300

    Final Linktree

---
+++ src/pages/index.tsx
@@ -0,0 +1,92 @@
+/* eslint-disable @next/next/no-img-element */
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.mainContent}>
+      <Head>
+        <title>DanLimaX</title>
+      </Head>
+      <div className={styles.socialContent}>
+        <div className={styles.profileContent}>
+          <img
+            src="danlimax.png"
+            alt="danlimax logo"
+            className={styles.imgLogo}
+          />
+          <h3 className={styles.titleSocial}>@danlimax</h3>
+          <h4 className={styles.contactTitle}>
+            Contato: <a href="mailto:">contatodanlimax@gmail.com</a>
+          </h4>
+        </div>
+        <div className={styles.socialButtons}>
+          <a
+            href=""
+            className={styles.socialLinks}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            BetBRX
+          </a>
+          <a
+            href="https://twitch.tv/danlimax"
+            className={styles.socialLinks}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitch
+          </a>
+          <a
+            href=""
+            className={styles.socialLinks}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Youtube
+          </a>
+          <a
+            href=""
+            className={styles.socialLinks}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href=""
+            className={styles.socialLinks}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TikTok
+          </a>
+          <a
+            href=""
+            className={styles.socialLinks}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Discord
+          </a>
+          <a
+            href=""
+            className={styles.socialLinks}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href=""
+            className={styles.socialLinks}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Koo
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
